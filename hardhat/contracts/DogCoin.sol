404c781412a71e6b2bd313e0075ca79facb30aa5
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2022-02-28 18:20:47 +0000

    Add dog coin holder array

--- hardhat/contracts/DogCoin.sol
+++ hardhat/contracts/DogCoin.sol
@@ -5,17 +5,40 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DogCoin is ERC20 {
-    string private greeting;
+    constructor() ERC20("DogCoin", "DOG") {}
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override {}
-    
+    address[] public holders;
+
     function _afterTokenTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal virtual override {}
+    ) internal virtual override {
+        _addHolderIfNotExists(to);
+        _deleteHolderIfBalanceZero(from);
+    }
+
+    function _addHolderIfNotExists(address account) internal {
+        for (uint256 i = 0; i < holders.length; i++) {
+            if (holders[i] == account) {
+                return;
+            }
+        }
+        holders.push(account);
+    }
+
+    function _deleteHolderIfBalanceZero(address account) internal {
+        if (balanceOf(account) != 0) {
+            return;
+        }
+
+        // swap and pop
+        for (uint256 i = 0; i < holders.length; i++) {
+            if (holders[i] == account) {
+                holders[i] = holders[holders.length - 1];
+                holders.pop();
+                return;
+            }
+        }
+    }
 }
